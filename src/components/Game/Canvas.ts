0032404c997c4e1c38d7dbd37dad5084dad0fcfe
Alejo Gustavo Francomano <alejo@kodacon.com>
2023-11-20 19:13:19 -0300

    refactor typesript file

--- src/components/Game/Canvas.ts
+++ src/components/Game/Canvas.ts
@@ -1,7 +1,8 @@
 
-export const drawCanvas = (myCanvas: string) => {
-    const canvas = document.getElementById(myCanvas);
-  
+export const drawCanvas = () => {
+
+  const canvas: HTMLCanvasElement | null = document.getElementById('myCanvas') as HTMLCanvasElement | null;
+
     if (canvas && canvas instanceof HTMLCanvasElement) {
       const ctx = canvas.getContext('2d');
   
@@ -11,26 +12,22 @@ export const drawCanvas = (myCanvas: string) => {
       
       // Dibujar el cuadrado en el canvas
       function drawSquare() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height); // Limpiar el canvas
-        ctx.fillStyle = 'red';
-        ctx.fillRect(squareX, squareY, squareSize, squareSize);
+        if (ctx) {
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
+          ctx.fillStyle = 'blue';
+          ctx.fillRect(squareX, squareY, squareSize, squareSize);
+        }
       }
 
-      function moveSquare(event) {
+      function moveSquare(event: { keyCode: any; }) {
         const speed = 5; // Velocidad de movimiento
       
         switch (event.keyCode) {
           case 37: // Flecha izquierda
             squareX -= speed;
             break;
-          case 38: // Flecha arriba
-            squareY -= speed;
-            break;
           case 39: // Flecha derecha
             squareX += speed;
-            break;
-          case 40: // Flecha abajo
-            squareY += speed;
             break;
         }
       
