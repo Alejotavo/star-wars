ce4240475a347986ebc0fe194c239f0253aa3ca1
Alejo Gustavo Francomano <alejo@kodacon.com>
2023-11-20 16:35:14 -0300

    add Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import "./App.css";
 import CharacterWrapper from "./components/Character-wrapper/Character-wrapper";
-import Container from "react-bootstrap/Container";
+import Game from "./components/Game/Game";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        <CharacterWrapper />
-      </Container>
+      <Router>
+        <Container>
+          <Nav>
+            <Nav.Item>
+              <Nav.Link href="/">Characters</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/game">Game</Nav.Link>
+            </Nav.Item>
+          </Nav>
+
+          <Routes>
+            <Route path="/" element={<CharacterWrapper />} />
+            <Route path="/game" element={<Game />} />
+          </Routes>
+        </Container>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Game/Game.tsx
@@ -0,0 +1,9 @@
+const Game = () => {
+  return (
+    <div>
+      <label>Star Wars Game!</label>
+    </div>
+  );
+};
+
+export default Game;
