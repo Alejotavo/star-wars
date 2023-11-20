2800720719bcb5bed584ef5613fc1ec35557d6ee
Alejo Gustavo Francomano <alejo@kodacon.com>
2023-11-20 18:20:12 -0300

    canvas move square

--- src/components/Game/Canvas.ts
+++ src/components/Game/Canvas.ts
@@ -5,16 +5,48 @@ export const drawCanvas = (myCanvas: string) => {
     if (canvas && canvas instanceof HTMLCanvasElement) {
       const ctx = canvas.getContext('2d');
   
-      if (ctx) {
-        const squareX = 50;
-        const squareY = 50;
-        const squareSize = 50;
-  
-        ctx.fillStyle = 'blue';
+      let squareX = 50;
+      let squareY = 50;
+      const squareSize = 50;
+      
+      // Dibujar el cuadrado en el canvas
+      function drawSquare() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height); // Limpiar el canvas
+        ctx.fillStyle = 'red';
         ctx.fillRect(squareX, squareY, squareSize, squareSize);
-      } else {
-        console.error('Canvas context not supported.');
       }
+
+      function moveSquare(event) {
+        const speed = 5; // Velocidad de movimiento
+      
+        switch (event.keyCode) {
+          case 37: // Flecha izquierda
+            squareX -= speed;
+            break;
+          case 38: // Flecha arriba
+            squareY -= speed;
+            break;
+          case 39: // Flecha derecha
+            squareX += speed;
+            break;
+          case 40: // Flecha abajo
+            squareY += speed;
+            break;
+        }
+      
+        drawSquare(); // Volver a dibujar el cuadrado después de cambiar las coordenadas
+      }
+
+      document.addEventListener('keydown', moveSquare);
+
+
+// Dibujar el cuadrado inicial
+drawSquare();
+
+
+
+
+
     } else {
       console.error('Canvas element');
     }
