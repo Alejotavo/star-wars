416be9e133cdd46e5644ec43d0ee05de4b6e17c8
Alejo Gustavo Francomano <alejo@kodacon.com>
2023-11-20 17:51:16 -0300

    canvas element

--- src/components/Game/Canvas.ts
+++ src/components/Game/Canvas.ts
@@ -2,16 +2,20 @@
 export const drawCanvas = (myCanvas: string) => {
     const canvas = document.getElementById(myCanvas);
   
-    if (canvas) {
+    if (canvas && canvas instanceof HTMLCanvasElement) {
       const ctx = canvas.getContext('2d');
   
-      const squareX = 50;
-      const squareY = 50;
-      const squareSize = 100;
+      if (ctx) {
+        const squareX = 50;
+        const squareY = 50;
+        const squareSize = 50;
   
-      ctx.fillStyle = 'blue';
-      ctx.fillRect(squareX, squareY, squareSize, squareSize);
+        ctx.fillStyle = 'blue';
+        ctx.fillRect(squareX, squareY, squareSize, squareSize);
+      } else {
+        console.error('Canvas context not supported.');
+      }
     } else {
-      console.error('Canvas element not found.');
+      console.error('Canvas element');
     }
   };
