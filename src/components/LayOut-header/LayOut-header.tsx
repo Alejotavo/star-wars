c34863bc671910b44d00fcd83715fd5870530aba
Alejotavo <alejogustavof@gmail.com>
2024-04-05 09:20:57 -0300

    responsive header

--- src/components/LayOut-header/LayOut-header.tsx
+++ src/components/LayOut-header/LayOut-header.tsx
@@ -7,19 +7,22 @@ import Navbar from "react-bootstrap/Navbar";
 const LayOutHeader = () => {
   return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark" className="mb-3">
+      <Navbar expand="lg" bg="dark" data-bs-theme="dark" className="mb-3">
         <Container fluid>
           <Navbar.Brand href="#">
             <LogoHeader />
           </Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/films">Films</Nav.Link>
-            <Nav.Link href="/starships">Starships</Nav.Link>
-            <Nav.Link href="/vehicles">Vehicles</Nav.Link>
-            <Nav.Link href="/species">Species</Nav.Link>
-            <Nav.Link href="/planets">Planets</Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/films">Films</Nav.Link>
+              <Nav.Link href="/starships">Starships</Nav.Link>
+              <Nav.Link href="/vehicles">Vehicles</Nav.Link>
+              <Nav.Link href="/species">Species</Nav.Link>
+              <Nav.Link href="/planets">Planets</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
